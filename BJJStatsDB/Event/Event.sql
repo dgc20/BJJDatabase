6b17e3af801bcc984f626b26f9b201366b6a35d1
DGC20 <dgc20@pitt.edu>
2024-10-18 16:49:11 -0400

    Add project files.

---
+++ BJJStatsDB/Event/Event.sql
@@ -0,0 +1,10 @@
+﻿CREATE Table [Event].[Event]
+	(
+	EventID int primary key identity(1,1)
+	, OrganziationName varchar(500),
+	City varchar(500),
+	State varchar(200),
+	Zip varchar(20),
+	EventDate date
+	)
+
